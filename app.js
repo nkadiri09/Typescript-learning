6e4b677d754d9800ea72adc0fa9d4edfa5755fd8
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2024-01-18 22:21:16 -0500

    Added types

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('HELLO WORLD this is just an example..');
+console.log("HELLO WORLD this is just an example..");
+function add(number1, number2) {
+    var name = "Narendra";
+    console.log(name);
+    return number1 + number2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var results = number1 + number2;
+var person = {
+    name: "Narendra",
+    hobbies: ["research", "cooking", "learning new things"],
+    age: 25,
+};
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+console.log(results);
--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log('HELLO WORLD this is just an example..');
+console.log("HELLO WORLD this is just an example..");
+
+function add(number1, number2) {
+  let name = "Narendra";
+  console.log(name);
+  return number1 + number2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const results = number1 + number2;
+
+const person = {
+  name: "Narendra",
+  hobbies: ["research", "cooking", "learning new things"],
+  age: 25,
+};
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
+
+console.log(results);
